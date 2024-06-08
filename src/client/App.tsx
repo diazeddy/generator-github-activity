5b2b61d1db2846e87d2a874dfe1f73a3db750773
Eddy Diaz <eddydiaz0219@gmail.com>
2024-06-08 18:23:52 -0500

    build post api and a form in the client

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,13 +1,47 @@
-import "./App.css";
+import { ChangeEvent, useState } from "react";
 
-import { useEffect, useState } from "react";
+import "./App.css";
+import { generate } from "./api";
 
 function App() {
+  const [startDate, setStartDate] = useState(new Date(2020, 7, 20, 0, 0, 0));
+  const [endDate, setEndDate] = useState(new Date(2020, 7, 21, 0, 0, 0));
+  const [commits, setCommits] = useState(5);
+  const [frequency, setFrequency] = useState(100);
+
+  const onChangeStartDate = (e: ChangeEvent<HTMLInputElement>) => {
+    const values = e.target.value.split("-");
+    setStartDate(new Date(Number(values[0]), Number(values[1]) - 1, Number(values[2]), 0, 0, 0));
+  }
+  const onChangeEndDate = (e: ChangeEvent<HTMLInputElement>) => {
+    const values = e.target.value.split("-");
+    setEndDate(new Date(Number(values[0]), Number(values[1]) - 1, Number(values[2]), 0, 0, 0));
+  }
+  const onChangeCommits = (e: ChangeEvent<HTMLInputElement>) => {
+    setCommits(Number(e.target.value));
+  }
+  const onChangeFrequency = (e: ChangeEvent<HTMLInputElement>) => {
+    setFrequency(Number(e.target.value));
+  }
+  const onClickGenerate = async () => {
+    if (startDate > endDate || commits < 0 || frequency < 0) {
+      alert("Invalid data");
+    }
+    await generate(startDate, endDate, commits, frequency);
+  }
 
   return (
-    <div className="grid grid-cols-3 gap-4">
-      React + TS + TailwindCSS + Express
-    </div>
+    <section>
+      <label>Start Date:</label>
+      <input type="date" value={startDate.toISOString().slice(0, 10)} onChange={onChangeStartDate}  />
+      <label>End Date:</label>
+      <input type="date" value={endDate.toISOString().slice(0, 10)} onChange={onChangeEndDate} />
+      <label>Commits:</label>
+      <input type="number" value={commits} onChange={onChangeCommits} />
+      <label>Frequency:</label>
+      <input type="number" value={frequency} onChange={onChangeFrequency} />
+      <button onClick={onClickGenerate}>Generate</button>
+    </section>
   );
 }
 
---
+++ src/server/constants.ts
@@ -0,0 +1,6 @@
+import dotenv from "dotenv";
+
+dotenv.config()
+
+export const GIT_USERNAME: string = process.env.GIT_USERNAME ?? "";
+export const GIT_EMAIL: string = process.env.GIT_EMAIL ?? "";

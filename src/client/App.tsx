62a966a177dbd45f75f67b764633671b067a4077
Eddy Diaz <eddydiaz0219@gmail.com>
2024-06-09 17:10:37 -0500

    update the input fields and validation

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -7,6 +7,9 @@ function App() {
   const [commits, setCommits] = useState(10);
   const [frequency, setFrequency] = useState(50);
 
+  const MAX_FREQUENCY = 100;
+  const MAX_COMMIT = 100;
+
   const onChangeStartDate = (e: ChangeEvent<HTMLInputElement>) => {
     const values = e.target.value.split("-");
     setStartDate(new Date(Number(values[0]), Number(values[1]) - 1, Number(values[2]), 0, 0, 0));
@@ -22,7 +25,7 @@ function App() {
     setFrequency(Number(e.target.value));
   }
   const onClickGenerate = async () => {
-    if (startDate > endDate || commits < 0 || frequency < 0) {
+    if (startDate > endDate || commits < 0 || frequency < 0 || endDate > new Date() || commits > MAX_COMMIT || frequency > MAX_FREQUENCY) {
       alert("Invalid data");
     }
     await generate(startDate, endDate, commits, frequency);
@@ -35,21 +38,21 @@ function App() {
         <div className="grid grid-cols-2 gap-5">
           <div>
             <label className="block text-gray-700 text-sm font-bold mb-2">Start Date:</label>
-            <input type="date" value={startDate.toISOString().slice(0, 10)} onChange={onChangeStartDate} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+            <input type="date" value={startDate.toISOString().slice(0, 10)} onChange={onChangeStartDate} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required />
           </div>
           <div>
             <label className="block text-gray-700 text-sm font-bold mb-2">End Date:</label>
-            <input type="date" value={endDate.toISOString().slice(0, 10)} onChange={onChangeEndDate} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"  />
+            <input type="date" value={endDate.toISOString().slice(0, 10)} onChange={onChangeEndDate} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required />
           </div>
         </div>
         <div className="grid grid-cols-2 gap-5 mt-5">
           <div>
             <label className="block text-gray-700 text-sm font-bold mb-2">Commits:</label>
-            <input type="number" value={commits} onChange={onChangeCommits} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+            <input type="number" value={commits} onChange={onChangeCommits} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required />
           </div>
           <div>
             <label className="block text-gray-700 text-sm font-bold mb-2">Frequency:</label>
-            <input type="number" value={frequency} onChange={onChangeFrequency} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+            <input type="number" value={frequency} onChange={onChangeFrequency} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required />
           </div>
         </div>
         <button onClick={onClickGenerate} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-5">Generate</button>
